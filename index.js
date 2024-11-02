5ec789c055f7347637d9ad527e42dc0f533d8f02
Abdullah Al Mamun <101423827+hummerheads@users.noreply.github.com>
2024-11-02 12:03:12 +0600

    Added Cors

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173'],
+  origin: ['http://localhost:5173', 'https://foodies-restaurant-server-qbfedtxj6.vercel.app/'],
   credentials: true
 }));
 app.use(express.json()); // Ensure you can parse JSON requests
